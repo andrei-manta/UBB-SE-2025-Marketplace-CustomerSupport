241b1e517ea01e6ddab7a9afae570929fcc738fa
andrei-manta <andreimanta100@gmail.com>
2025-03-27 13:32:57 +0200

    Some progress on my functionality

--- BackendUserGetHelp.cs
+++ BackendUserGetHelp.cs
@@ -26,5 +26,52 @@ namespace Marketplace_SE
 
             return true;
         }
+
+        public static List<HelpTicket> LoadTicketsFromDB(List<string> TicketIDs)
+        {
+            List<HelpTicket> returnList = new List<HelpTicket>();
+
+            foreach(string each in TicketIDs)
+            {
+                //transform string to ID's format, call DB, construct HelpTicket, add to list
+            }
+
+            returnList.Add(new HelpTicket("0001", "1000", "User Name", "12-12-2022-09-22", "Lorem ipsum"));
+
+            returnList.Add(new HelpTicket("0002", "1000", "User Name", "12-12-2022-10-22", "Lorem ipsum"));
+
+            return returnList;
+        }
+
+        public static List<string> GetTicketIDsMatchingCriteria(string UserID, string DateAndTime, bool ExactDate, bool StartingDate, bool EndingDate)
+        {
+            List<string> returnList = new List<string>();
+
+            //lookup in the DB and add to list
+
+            return returnList;
+        }
+    }
+
+    public class HelpTicket
+    {
+        private string TicketID { get; }
+        private string UserID { get; }
+        private string UserName { get; }
+        private string DateHour { get; }
+        private string Description { get; }
+        public HelpTicket(string TicketID_, string UserID_, string UserName_, string DateHour_, string Description_)
+        {
+            TicketID = TicketID_;
+            UserID = UserID_;
+            UserName = UserName_;
+            DateHour = DateHour_;
+            Description = Description_;
+        }
+
+        public string toStringExceptDescription()
+        {
+            return "Ticket nr: " + TicketID.ToString() + "; User ID: " + UserID.ToString() + "; User name: " + UserName.ToString() + "; Date and time: " + DateHour.ToString();
+        }
     }
 }
