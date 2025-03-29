aaef9a62f6c4f8eee87bb0ac27fed0001b650368
914/2_Maftei_Razvan_Gabriel <147400905+MafteiRazvanGabriel@users.noreply.github.com>
2025-03-29 16:56:05 +0200

    Add files via upload

--- ReturnItemPage.xaml.cs
+++ ReturnItemPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,7 +19,7 @@ using Windows.UI.Notifications;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Marketplace_SE
+namespace Return_Item
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
